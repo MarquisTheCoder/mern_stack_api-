252380ddfa13e2b5457cbfef46129edc8a70f43b
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-01-24 21:26:24 -0600

    autosave script

--- backend /server.js
+++ backend /server.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+//setting up the initial route for the api reuest meant for goals
+app.get("/api/goals", (request, response) => { 
+    response.json({"command": "get goals"});
+})
 app.listen(port, () => console.log(`server starting localhost@${port}`));
 
 
