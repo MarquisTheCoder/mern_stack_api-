ead8d7ff8f60e9ea31193af28fa77a3f98b6c878
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-01-25 15:32:24 -0600

    autosave script

--- backend /routes/goalRoutes.js
+++ backend /routes/goalRoutes.js
@@ -3,7 +3,22 @@ const router = express.Router();
 
 //setting up the initial route for the api reuest meant for goals
 router.get("/", (request, response) => { 
-    response.status(200).json({"command": "get goals"});
-})
+    response.status(200).json({"command": "get goal"});
+});
+
+//setting up the creation of current post
+router.post("/", (request, response)=> {
+    response.status(200).json({"command": "set goal"});
+});
+
+//setting up the editing and changing of a particular goal by id
+router.put("/:id", (request, response) => {
+    response.status(200).json({"command": `change goal ${request.param.id}`});
+});
+
+//setting up tje deletion of a [articular goal by id
+router.delete("/:id", (request, response) => {
+    response.status(200).json({"command" : `delete goal ${request.param.id}`});
+});
 
 module.exports = router;
