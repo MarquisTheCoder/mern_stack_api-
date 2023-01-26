c2c74939ccc03ebf1aadb6bcf4263de9b27cc6d9
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-01-25 21:55:59 -0600

    refactoring the controllers and routes for the Rest API

---
+++ backend /controllers/goalController.js
@@ -0,0 +1,27 @@
+
+//creating a controller class to contain the controller for 
+//each route contained in goalRoutes.js 
+const GoalController = {
+
+    //theses four functions represent the for common utilities of a
+    //REST API C.R.U.D( create, read, update, delete)
+    getGoals : (request, response) => {
+        response.status(200).json({"command" : "get goal"})
+    },
+
+    setGoals : (request, response) => {
+        response.status(200).json({"command" : "post goal"});
+    },
+
+    changeGoal : (request, response) => {
+        response.status(200).json({"command": `changing goal ${request.param.id}`});
+    },
+
+    deleteGoal : (request, response) => {
+        response.status(200).json({"command" : `deleting goal ${request.param.id}`});
+    }
+}
+
+module.exports = {
+    GoalController,
+}
--- backend /routes/goalRoutes.js
+++ backend /routes/goalRoutes.js
@@ -1,28 +1,14 @@
 
-
+const GoalController = require("../controllers/goalController");
 const express = reuqire("express");
-
-//creating router object to be used in server.js
 const router = express.Router();
 
-//setting up the initial route for the api reuest meant for goals
-router.get("/", (request, response) => { 
-    response.status(200).json({"command": "get goal"});
-});
+router.route("/")
+    .get(GoalController.getGoals)
+    .post(GoalController.setGoals);
 
-//setting up the creation of current post
-router.post("/", (request, response)=> {
-    response.status(200).json({"command": "set goal"});
-});
-
-//setting up the editing and changing of a particular goal by id
-router.put("/:id", (request, response) => {
-    response.status(200).json({"command": `change goal ${request.param.id}`});
-});
-
-//setting up tje deletion of a [articular goal by id
-router.delete("/:id", (request, response) => {
-    response.status(200).json({"command" : `delete goal ${request.param.id}`});
-});
+router.route("/:id")
+    .put(GoalController.setGoals)
+    .delete(GoalController.deleteGoals);
 
 module.exports = router;
