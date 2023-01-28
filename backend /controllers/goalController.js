a0b09091d26b2e4b2bf2d2a2ceb390180e3af464
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-01-28 12:30:30 -0600

    adding conditional to the getGoals controller

--- backend /controllers/goalController.js
+++ backend /controllers/goalController.js
@@ -2,10 +2,23 @@
 //creating a controller class to contain the controller for 
 //each route contained in goalRoutes.js 
 class GoalController{
+
     //theses four functions represent the for common utilities of a
     //REST API C.R.U.D( create, read, update, delete)
+
     getGoals(request, response){
-        response.status(200).json({"command" : "get goal"})
+        if(request.body.text){
+
+            //only outputting the request.bodies text if it actually
+            //exist this conditional is there to prevent the attempt
+            //of trying to access a null value
+            console.log(`text: ${request.body.text}`);
+            response.status(200).json({"command" : "get goal"});
+        }else{
+            console.log(`No request body found`);
+            response.status(400).json({"command" : "request body not found"});
+        }
+
     }
 
     setGoals(request, response){
